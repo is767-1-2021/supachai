94747e98057edce232e1a7164321998e1846b685
Supachai Pantaweesak <supachai-pan63@tbs.tu.ac.th>
2021-10-24 22:44:38 +0700

    Todos with http,services, controller

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:first_app/models/first_form_model.dart';
 import 'package:first_app/pages/eighth_page.dart';
+import 'package:first_app/pages/todo_page.dart';
+import 'package:first_app/services/services.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'controllers/todo.dart';
 import 'pages/fifth_page.dart';
 import 'pages/first_page.dart';
 import 'pages/forth_page.dart';
@@ -12,16 +15,24 @@ import 'pages/third_page.dart';
 import 'pages/seventh_page.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => FirstFormModel(),
-        ),
-      ],
-      child: MyApp(),
-    ),
-  );
+  var services = HttpServices();
+  var controller = TodoController(services);
+
+  runApp(TodoApp(controller: controller));
+}
+
+class TodoApp extends StatelessWidget {
+  final TodoController controller;
+  TodoApp({required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TodoPage(
+        controller: controller,
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
